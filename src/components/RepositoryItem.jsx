ef6069a22a3093c5ebaae3076f491ea555880097
Zenrer <ram_idts@me.com>
2022-06-29 18:42:34 +1000

    finished part 10 exercise 3

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+const RepositoryItem = ({ item }) => {
+  const { description, forksCount, fullName, language, ratingAverage, reviewCount, stargazersCount } = item;
+
+  return (
+    <View>
+      <Text>Full name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
