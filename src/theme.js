7235efccfa74f58ef59868c7e4d9f1a647bf63f7
Zenrer <ram_idts@me.com>
2022-06-30 10:22:51 +1000

    finished part 10 exercise 10

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: "#24292e",
@@ -5,14 +7,18 @@ const theme = {
     primary: "#0366d6",
     appBarBackground: "#24292e",
     background: "#e1e4e8",
-    error: "#d73a4a"
+    error: "#d73a4a",
   },
   fontSizes: {
     body: 14,
     subheading: 16,
   },
   fonts: {
-    main: "System",
+    main: Platform.select({
+      android: "Roboto",
+      ios: "Arial",
+      default: "System",
+    }),
   },
   fontWeights: {
     normal: "400",
