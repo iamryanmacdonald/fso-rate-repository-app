daa2dc07ed801616b69f67a821dbfbae5aba22ee
Zenrer <ram_idts@me.com>
2022-06-30 11:06:08 +1000

    finished part 10 exercise 11

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,13 @@
+import { useQuery } from "@apollo/client";
+
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  return { loading, repositories: data ? data.repositories : null };
+};
+
+export default useRepositories;
